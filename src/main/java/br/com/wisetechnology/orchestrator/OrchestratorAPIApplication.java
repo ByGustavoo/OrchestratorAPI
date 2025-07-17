874a07056cc90be464edd4013f18ea0aecd94d26
Gustavo <gustavo.ccx@hotmail.com>
2025-07-16 21:58:51 -0300

    OCT-1
    - Commit de Inicialização

---
+++ src/main/java/br/com/wisetechnology/orchestrator/OrchestratorAPIApplication.java
@@ -0,0 +1,12 @@
+package br.com.wisetechnology.orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratorAPIApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/wisetechnology/orchestrator/OrchestratorAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wisetechnology.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
